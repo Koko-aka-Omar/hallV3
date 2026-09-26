5e60df98b13237f7dcd8555ece58b3b834eb2048
Koko aka Omar <144022997+Koko-aka-Omar@users.noreply.github.com>
2026-09-26 16:04:04 +0400

    Align checkpoint footprints to official map

--- M7A_GitHub_Website_Full_Resolution/m7a-building/halls.js
+++ M7A_GitHub_Website_Full_Resolution/m7a-building/halls.js
@@ -1,11 +1,19 @@
-// One outdoor pin per building. Coordinates are [longitude, latitude].
-// A tour can open an existing scene or a separately hosted hall tour.
+// Outdoor checkpoints on the official campus-map image.
+// coordinates = real tour location; mapCoordinates/mapOutline = visual placement on the map image.
 export const HALLS = [
   {
     id: 'e2', code: 'E2',
     name: { en: 'Al Razi Auditorium', ar: 'مسرح الرازي' },
     coordinates: [55.477603577553126, 25.27433935800182],
-    mapCoordinates: [55.470176545034015, 25.27991428984133],
+    mapCoordinates: [55.470730009539956, 25.27982859846141],
+    mapOutline: [
+      [55.47101662508768, 25.279522632920838],
+      [55.47083349344968, 25.279506861501222],
+      [55.470374210928995, 25.27992217555114],
+      [55.470374210928995, 25.2800720040375],
+      [55.47105150730444, 25.280119318296354],
+      [55.47101662508768, 25.279522632920838]
+    ],
     thumbnail: './panoramas-mobile/theater-outer-entrance-a.jpg',
     tour: { scene: 'theater-entrance-a' },
     rooms: []
@@ -14,7 +22,14 @@ export const HALLS = [
     id: 'm7', code: 'M7',
     name: { en: 'College of Science', ar: 'كلية العلوم' },
     coordinates: [55.47720532173917, 25.2857153181386],
-    mapCoordinates: [55.482757397879176, 25.28027440392259],
+    mapCoordinates: [55.482802454075824, 25.280271775352652],
+    mapOutline: [
+      [55.48292599526019, 25.280374289580163],
+      [55.482678912891465, 25.280374289580163],
+      [55.482678912891465, 25.28016926112514],
+      [55.48292599526019, 25.28016926112514],
+      [55.48292599526019, 25.280374289580163]
+    ],
     thumbnail: './panoramas-mobile/ground-entrance.jpg',
     tour: { scene: 'entrance' },
     rooms: [
