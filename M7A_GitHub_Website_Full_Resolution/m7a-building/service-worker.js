20d3da8c60ffe87f8aa164e3b13220e3c351de6c
Koko aka Omar <144022997+Koko-aka-Omar@users.noreply.github.com>
2026-09-26 15:03:51 +0400

    Cache official campus map asset

--- M7A_GitHub_Website_Full_Resolution/m7a-building/service-worker.js
+++ M7A_GitHub_Website_Full_Resolution/m7a-building/service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_PREFIX='m7a-tour:'+self.registration.scope+':';
-const SHELL_CACHE=CACHE_PREFIX+'shell-v36';
+const SHELL_CACHE=CACHE_PREFIX+'shell-v37';
 // Bump only when panorama files change; UI releases retain full-resolution downloads.
 const PANORAMA_CACHE=CACHE_PREFIX+'panoramas-v3';
 const LEGACY_CACHE='m7a-tour-v7';
-const CORE_ASSETS=['./','./index.html','./halls.js','./campus-directory.js','./directions.js','./favicon.svg','./manifest.webmanifest','./apple-touch-icon.png','./social-preview.png'];
+const CORE_ASSETS=['./','./index.html','./halls.js','./campus-directory.js','./directions.js','./favicon.svg','./manifest.webmanifest','./apple-touch-icon.png','./social-preview.png','./campus-map-2026.webp'];
 const PANORAMA_NAMES=[
   'ground-entrance','ground-study-rooms','ground-hall-end',
   'top-stair-landing','top-faculty-offices','top-seating-area',
